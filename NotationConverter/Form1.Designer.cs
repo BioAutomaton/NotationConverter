a7b34f4e9b01293512b2ca8b3ad0e0130835dba0
Michael Potyomkin <54860902+BioAutomaton@users.noreply.github.com>
2020-09-09 21:47:46 +0300

    Minor changes

--- NotationConverter/Form1.Designer.cs
+++ NotationConverter/Form1.Designer.cs
@@ -167,7 +167,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Notation Converter";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
